80d897b1419ee20ad8ccfc6cf984244e1d3a2ba0
Kuan-Fu <creazy_fu@hotmail.com>
2022-04-07 00:30:13 +0800

    modify:libjs

--- WEEK1/lib.js
+++ WEEK1/lib.js
@@ -2,6 +2,7 @@ const headers = {
   'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Methods': 'PATCH, POST, GET,OPTIONS,DELETE',
+  // 各種狀況
   'Content-Type': 'application/json'
 }
 const status={
